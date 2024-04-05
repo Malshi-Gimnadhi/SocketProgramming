72b76eec23662653d6acea8ce3f1875aa8f14546
Malshi Gimnadhi <139608397+Malshi-Gimnadhi@users.noreply.github.com>
2024-04-05 11:05:58 +0530

    Add files via upload

---
+++ day2/src/day2/UDPClient.java
@@ -0,0 +1,22 @@
+package day2;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPClient {
+	public static void main(String[]args) throws IOException{
+		DatagramSocket client = new  DatagramSocket();
+		InetAddress ipAdress = InetAddress.getLocalHost();
+		String str="Hello World";
+		byte[]  buf =str.getBytes();
+		
+		DatagramPacket pac =new DatagramPacket(buf, buf.length, ipAdress,4222);
+		client.send(pac);
+		client.close();
+		
+	}
+	
+
+}
